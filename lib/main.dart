fbb7c68f577c3d6aaa2c8f6a25488b66544fe34c
George <giorgi.bibilashvili89@gmail.com>
2020-10-09 22:48:46 +0400

    Create multimle buttons and onPress events

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,39 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion(String answer) {
+    print(answer);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    List<String> question = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text(question.elementAt(0)),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: () => answerQuestion('one'),
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () => answerQuestion('two'),
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: () => answerQuestion('tree'),
+          )
+        ],
+      ),
+    ));
   }
 }
